62d74b8f76e94141993283239148086d17b82218
desprado2 <46255627+desprado2@users.noreply.github.com>
2020-07-24 14:17:19 +0800

    add some test functions in SearchController

--- src/main/java/app/ticket/controller/SearchController.java
+++ src/main/java/app/ticket/controller/SearchController.java
@@ -4,11 +4,15 @@ import app.ticket.entity.Section;
 import app.ticket.entity.Ticket;
 import app.ticket.service.TicketService;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSON;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.text.SimpleDateFormat;
 
 @RestController
 @RequestMapping("/search")
@@ -19,28 +23,96 @@ public class SearchController {
         this.ticketService = ticketService;
     }
 
+    private class myClass{
+        public Ticket t;
+        public Integer matchDeg;
+        public myClass(Ticket t, Integer matchDeg){
+            super();
+            this.t = t;
+            this.matchDeg = matchDeg;
+        }
+    }
+    private class SortByMatchDeg implements Comparator<myClass> {
+        @Override
+        public int compare(myClass o1, myClass o2) {
+            return -(o1.matchDeg > o2.matchDeg ? 1 : o1.matchDeg == o2.matchDeg? 0 : -1); //descending order
+        }
+    }
+    private class SortByStartDate implements Comparator<myClass> {
+        @Override
+        public int compare(myClass o1, myClass o2) {
+            return (o1.t.getStartDate().compareTo(o2.t.getStartDate())); //descending order
+        }
+    }
+
     @GetMapping
-    public List<JSONObject> getSearch(@RequestParam(value="s") String text) {
+    public ResponseEntity<String> getSearch(@RequestParam(value = "s") String text,
+                                            @RequestParam(value = "city", defaultValue = "") String city,
+                                            @RequestParam(value = "cat", defaultValue = "") String category,
+                                            @RequestParam(value = "st", defaultValue = "") String startDate,
+                                            @RequestParam(value = "en", defaultValue = "") String endDate,
+                                            @RequestParam(value = "o", defaultValue = "2") Integer orderChosen) {
+        System.out.println("Search : " + text);
+        if (orderChosen < 1 || orderChosen > 3){
+            JSONObject res = new JSONObject();
+            res.put("code", -1);
+            res.put("message", "Bad Request");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res.toJSONString());
+        }
+
         List<Ticket> ticketList = ticketService.findAll();
         String sText[] = text.split(" ");
-        List<Ticket> res[] = new List[10];
-        for (int i = 0; i < 10; i++)
-            res[i] = new ArrayList<>();
+        SimpleDateFormat ft = new SimpleDateFormat ("yyyyMMdd");
+        Date st = new Date(), en = new Date();
+        if (!startDate.isBlank() && !endDate.isBlank()) {
+            //System.out.println("st = " + startDate + ";  en = " + endDate);
+            try {
+                st = ft.parse(startDate);
+                en = ft.parse(endDate);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                System.err.println("ERROR: Failed to parse date string");
+            }
+        }
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(en);
+        calendar.add(Calendar.HOUR, 23);
+        calendar.add(Calendar.MINUTE, 59);
+        calendar.add(Calendar.SECOND, 59);
+        en = calendar.getTime();
+
+        List<myClass> res = new ArrayList<>();
         for (Ticket t : ticketList){
+            if (city != "" && !t.getCity().equals(city)) continue;
+            if (category != "" && !t.getCategory().equals(category)) continue;
+            if (!startDate.isBlank() && !endDate.isBlank()){
+                Date tst = t.getStartDate(), ten = t.getEndDate();
+                if (!((tst.compareTo(st) >= 0 && tst.compareTo(en) <= 0) ||
+                      (ten.compareTo(st) >= 0 && ten.compareTo(en) <= 0) ||
+                      (st.compareTo(tst) >= 0 && st.compareTo(ten) <= 0) ||
+                      (en.compareTo(tst) >= 0 && en.compareTo(ten) <= 0)) )
+                    continue;
+            }
+
             Integer cnt = 0;
             for (String s : sText){
                 if (s.length() == 0) continue;
                 if (t.getName().contains(s) || t.getPlace().contains(s) || t.getCategory().contains(s) || t.getCity().contains(s))
                     cnt++;
             }
-            res[cnt].add(t);
+            if (cnt == 0) continue;
+            res.add(new myClass(t, cnt));
         }
+
+        if (orderChosen == 1)
+            Collections.sort(res, new SortByMatchDeg());
+        else if (orderChosen == 3)
+            Collections.sort(res, new SortByStartDate());
+
         List<JSONObject> j = new ArrayList<>();
-        for (int i = 9; i > 0; i--) {
-            for (Ticket t : res[i])
-                j.add(wrapTicket(t));
-        }
-        return j;
+        for (myClass r : res)
+            j.add(wrapTicket(r.t));
+        return ResponseEntity.ok(JSON.toJSONString(j));
     }
 
     private JSONObject wrapTicket(Ticket ticket) {
