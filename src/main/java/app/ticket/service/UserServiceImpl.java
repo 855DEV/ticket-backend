0a24a9860b4eeca0e947eaf1517b3615f467e8e1
SeanChao <seanchao0804@gmail.com>
2020-07-28 15:10:28 +0800

    test: user controller 100% 🎉

--- src/main/java/app/ticket/service/UserServiceImpl.java
+++ src/main/java/app/ticket/service/UserServiceImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private UserDao userDao;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserDao userDao;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public UserServiceImpl(UserDao userDao,
                            BCryptPasswordEncoder bCryptPasswordEncoder) {
